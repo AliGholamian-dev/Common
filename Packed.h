be782ea86264e474d5239fabae2686bb3c2f6ee7
Sepehr <alisepehr88@gmail.com>
2026-02-01 20:29:09 +0100

    Added CMakeList.txt
    Signed-off-by: Sepehr <alisepehr88@gmail.com>

--- Packed.h
+++ Packed.h
@@ -6,13 +6,13 @@
 Common_NameManglingGuard_Begin
 
 #if defined(Common_Compiler_GCC) || defined(Common_Compiler_CLang)
-    #define Packed_Struct_Start
-    #define Packed_Struct_End
-    #define Packed_Struct_Attribute __attribute__((packed))
+    #define Common_Packed_Struct_Start
+    #define Common_Packed_Struct_End
+    #define Common_Packed_Struct_Attribute __attribute__((packed))
 #else
-    #define Packed_Struct_Start
-    #define Packed_Struct_End
-    #define Packed_Struct_Attribute
+    #define Common_Packed_Struct_Start
+    #define Common_Packed_Struct_End
+    #define Common_Packed_Struct_Attribute
     #error "[ Packing ]: Packing not supported or unknown compiler"
 #endif
 
