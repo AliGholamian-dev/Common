21aed66d84028d1171082f6051c88510a73028ec
Sepehr <alisepehr88@gmail.com>
2026-05-10 17:49:03 +0200

    Added compiler and platform enums
    Signed-off-by: Sepehr <alisepehr88@gmail.com>

--- Include/Common/CompilerDetection.h
+++ Include/Common/CompilerDetection.h
@@ -4,15 +4,41 @@
 
 Common_NameManglingGuard_Begin
 
-#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
-    #define Common_Compiler_GCC
+typedef enum Compiler
+{
+    Compiler_Unknown,
+    Compiler_Intel,
+    Compiler_Clang,
+    Compiler_GCC,
+    Compiler_MSVC,
+} Compiler;
+
+#if defined(__INTEL_COMPILER) || defined(__INTEL_LLVM_COMPILER)
+    #define Common_Compiler_Intel
 #elif defined(__clang__)
-    #define Common_Compiler_CLang
+    #define Common_Compiler_Clang
+#elif defined(__GNUC__)
+    #define Common_Compiler_GCC
 #elif defined(_MSC_VER)
     #define Common_Compiler_MSVC
 #else
     #define Common_Compiler_Unknown
     #error "[ Compiler ]: Unknown compiler"
 #endif
+
+static inline Compiler Common_GetCompiler(void)
+{
+    #if defined(Common_Compiler_Intel)
+        return Compiler_Intel;
+    #elif defined(Common_Compiler_Clang)
+        return Compiler_Clang;
+    #elif defined(Common_Compiler_GCC)
+        return Compiler_GCC;
+    #elif defined(Common_Compiler_MSVC)
+        return Compiler_MSVC;
+    #else
+        return Compiler_Unknown;
+    #endif
+}
 
 Common_NameManglingGuard_End
--- Include/Common/PlatformDetection.h
+++ Include/Common/PlatformDetection.h
@@ -4,15 +4,43 @@
 
 Common_NameManglingGuard_Begin
 
-#if defined(_WIN32) && defined(_WIN64)
+typedef enum Platform
+{
+    Platform_Unknown,
+    Platform_Windows32,
+    Platform_Windows64,
+    Platform_Linux,
+    Platform_Android,
+} Platform;
+
+#if defined(_WIN64)
     #define Common_Platform_Windows64
+    #define Common_Platform_Windows
 #elif defined(_WIN32)
     #define Common_Platform_Windows32
-#elif defined(__linux__) && !defined(__ANDROID__)
+    #define Common_Platform_Windows
+#elif defined(__ANDROID__)
+    #define Common_Platform_Android
+#elif defined(__linux__)
     #define Common_Platform_Linux
 #else
     #define Common_Platform_Unknown
     #error "[ Platform ]: Unknown platform"
 #endif
+
+static inline Platform Common_GetPlatform(void)
+{
+    #if defined(Common_Platform_Windows64)
+        return Platform_Windows64;
+    #elif defined(Common_Platform_Windows32)
+        return Platform_Windows32;
+    #elif defined(Common_Platform_Android)
+        return Platform_Android;
+    #elif defined(Common_Platform_Linux)
+        return Platform_Linux;
+    #else
+        return Platform_Unknown;
+    #endif
+}
 
 Common_NameManglingGuard_End
